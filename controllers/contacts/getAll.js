e0c9f0d112839b04ad1726c77cda67f6cf187420
VikaVelhan <velganvika@gmail.com>
2023-05-09 00:22:33 +0300

    update getAll

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,10 +2,25 @@ const Contact = require("../../models/contact");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
+
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "", { skip, limit });
-  res.json(result);
+
+  if (favorite) {
+    const result = await Contact.find({ owner, favorite }, "", {
+      skip,
+      limit,
+    });
+    res.json(result);
+  }
+
+  if (!favorite) {
+    const result = await Contact.find({ owner }, "", {
+      skip,
+      limit,
+    });
+    res.json(result);
+  }
 };
 
 module.exports = getAll;
